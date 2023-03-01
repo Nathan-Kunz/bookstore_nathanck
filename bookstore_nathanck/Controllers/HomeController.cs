91ff3ef850eebf7ee2da8db112fc514a486228c2
Nathan Kunz <nathanck@byu.edu>
2023-03-01 15:42:03 -0700

    Add project files.

---
+++ bookstore_nathanck/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using bookstore_nathanck.Models;
+using bookstore_nathanck.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookstore_nathanck.Controllers
+{
+    public class HomeController : Controller
+    {
+        private iBookStoreRepository repo;
+        public HomeController(iBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum =1)
+        {
+            int pageSize = 10;
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                //.OrderBy(b => b.Price)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPAge = pageNum
+                }
+            };
+            
+            return View(x);
+        }
+   
+    }
+}
